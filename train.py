e40ad365777151cecea1a0afd5c4934f0270ba42
dairui <dairui@didichuxing.com>
2017-05-27 17:03:35 +0800

    Fix session error

--- train.py
+++ train.py
@@ -48,8 +48,7 @@ def main():
     logging.info("Number of training examples per batch: {0},\n"
                  "Number of batches per epoch: {1}".format(config.batch_size,
                                                            num_batches))
-    sv = tf.train.Supervisor(logdir="/tmp/tb_logs")
-    with sv.managed_session() as sess:
+    with tf.Graph().as_default():
         print 'creating model...'
         sent_input = SentimentInput(config, data)
         with tf.name_scope("Train"):
@@ -63,62 +62,65 @@ def main():
                 m_valid = SentimentModel(config, sent_input)
             tf.summary.scalar("Validation Loss", m_valid.mean_loss)
             tf.summary.scalar("Validation Accuracy", m_valid.accuracy)
-        sess.run(tf.global_variables_initializer())
-        # model = create_model(config, sess)
-        learning_rate = config.learning_rate
-        lr_decay = config.lr_decay
-        print 'model creation completed.'
-        # train model and save to checkpoint
-        print "Training started..."
-        print "Maximum number of epochs to train for: {0}".format(config.max_epoch)
-        print "Batch size: {0}".format(config.batch_size)
-        print "Starting learning rate: {0}".format(config.learning_rate)
-        print "Learning rate decay factor: {0}".format(config.lr_decay)
+        initializer = tf.global_variables_initializer()
+        sv = tf.train.Supervisor(logdir="/tmp/tb_logs")
+        with sv.managed_session() as sess:
+            sess.run(initializer)
+            # model = create_model(config, sess)
+            learning_rate = config.learning_rate
+            lr_decay = config.lr_decay
+            print 'model creation completed.'
+            # train model and save to checkpoint
+            print "Training started..."
+            print "Maximum number of epochs to train for: {0}".format(config.max_epoch)
+            print "Batch size: {0}".format(config.batch_size)
+            print "Starting learning rate: {0}".format(config.learning_rate)
+            print "Learning rate decay factor: {0}".format(config.lr_decay)
 
-        step_time, loss = 0.0, 0.0
-        previous_losses = []
-        # Total number of batches to pass through.
-        tot_steps = num_batches * config.max_epoch
-        # starting at step 1 to prevent test set from running after first batch
-        for step in xrange(1, tot_steps):
-            # Get a batch and make a step.
-            start_time = time.time()
+            step_time, loss = 0.0, 0.0
+            previous_losses = []
+            # Total number of batches to pass through.
+            tot_steps = num_batches * config.max_epoch
+            # starting at step 1 to prevent test set from running after first batch
+            for step in xrange(1, tot_steps):
+                # Get a batch and make a step.
+                start_time = time.time()
 
-            inputs, targets, seq_lengths = sent_input.get_batch()
-            step_loss, _, accuracy = model.step(sess, inputs, targets, seq_lengths, True)
-            steps_per_checkpoint = 100
-            step_time += (time.time() - start_time) / steps_per_checkpoint
-            loss += step_loss / steps_per_checkpoint
+                inputs, targets, seq_lengths = sent_input.get_batch()
+                step_loss, _, accuracy = model.step(sess, inputs, targets, seq_lengths, True)
+                steps_per_checkpoint = 100
+                step_time += (time.time() - start_time) / steps_per_checkpoint
+                loss += step_loss / steps_per_checkpoint
 
-            # Once in a while, we run evals.
-            if step % steps_per_checkpoint == 0:
-                # Print statistics for the previous epoch.
-                print ("global step %d learning rate %.7f step-time %.2f loss %.4f"
-                       % (model.global_step.eval(), model.learning_rate.eval(),
-                          step_time, loss))
-                # Decrease learning rate if no improvement was seen over last 3 times.
-                if len(previous_losses) > 2 and loss > max(previous_losses[-3:]):
-                    # sess.run(model.learning_rate_decay_op)
-                    learning_rate *= lr_decay
-                    model.assign_lr(sess, learning_rate)
-                previous_losses.append(loss)
-                # Save checkpoint and zero timer and loss.
-                step_time, loss, test_accuracy = 0.0, 0.0, 0.0
-                # Run evals on test set and print their accuracy.
-                print "Running test set"
-                for _ in xrange(len(m_valid.input_data.test_data)):
-                    inputs, targets, seq_lengths = sent_input.get_batch(False)
-                    test_loss, _, accuracy = m_valid.step(
-                        sess, inputs, targets, seq_lengths, False)
-                    loss += test_loss
-                    test_accuracy += accuracy
-                normalized_test_loss, normalized_test_accuracy = loss / len(
-                    m_valid.input_data.test_data), test_accuracy / len(m_valid.input_data.test_data)
-                print "Avg Test Loss: {0}, Avg Test Accuracy: {1}".format(
-                    normalized_test_loss, normalized_test_accuracy)
-                print "-------Step {0}/{1}------".format(step, tot_steps)
-                loss = 0.0
-                sys.stdout.flush()
+                # Once in a while, we run evals.
+                if step % steps_per_checkpoint == 0:
+                    # Print statistics for the previous epoch.
+                    print ("global step %d learning rate %.7f step-time %.2f loss %.4f"
+                           % (model.global_step.eval(), model.learning_rate.eval(),
+                              step_time, loss))
+                    # Decrease learning rate if no improvement was seen over last 3 times.
+                    if len(previous_losses) > 2 and loss > max(previous_losses[-3:]):
+                        # sess.run(model.learning_rate_decay_op)
+                        learning_rate *= lr_decay
+                        model.assign_lr(sess, learning_rate)
+                    previous_losses.append(loss)
+                    # Save checkpoint and zero timer and loss.
+                    step_time, loss, test_accuracy = 0.0, 0.0, 0.0
+                    # Run evals on test set and print their accuracy.
+                    print "Running test set"
+                    for _ in xrange(len(m_valid.input_data.test_data)):
+                        inputs, targets, seq_lengths = sent_input.get_batch(False)
+                        test_loss, _, accuracy = m_valid.step(
+                            sess, inputs, targets, seq_lengths, False)
+                        loss += test_loss
+                        test_accuracy += accuracy
+                    normalized_test_loss, normalized_test_accuracy = loss / len(
+                        m_valid.input_data.test_data), test_accuracy / len(m_valid.input_data.test_data)
+                    print "Avg Test Loss: {0}, Avg Test Accuracy: {1}".format(
+                        normalized_test_loss, normalized_test_accuracy)
+                    print "-------Step {0}/{1}------".format(step, tot_steps)
+                    loss = 0.0
+                    sys.stdout.flush()
 
 
 if __name__ == '__main__':
